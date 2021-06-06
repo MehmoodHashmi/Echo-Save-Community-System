99b38a4f6d5697f72d23c8e6705bd7af396da828
soni <mehmood_hashmi2009@outlook.com>
2021-06-06 22:50:02 +0500

    commit 8

--- Echo Save Domain/tests/entities/SubmissionTest.java
+++ Echo Save Domain/tests/entities/SubmissionTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import enumerations.StateOfSubmission;
+import enumerations.StateOfSubmission; 
 
 
 /**
